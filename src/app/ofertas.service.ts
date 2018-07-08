c7f155a98821438fe2c153344be64cbfa828511b
Felipe Marchant <felipemarchant@outlook.com>
2018-07-08 01:04:23 -0300

    Add assets

---
+++ src/app/ofertas.service.ts
@@ -0,0 +1,11 @@
+export class OfertasServices {
+
+    public getOfertas():Array<string>{
+        let ofertas: Array<string> = [
+            'Oferta1',
+            'Oferta2',
+            'Oferta3',
+        ];
+        return ofertas;
+    }
+}
