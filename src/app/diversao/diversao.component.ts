1f1df3e9dc2fde3262fd221fc5bdd51dbe7cb727
Felipe Marchant <felipemarchant@outlook.com>
2018-07-12 21:35:00 -0300

    Fetch Diversão na Categoria

--- src/app/diversao/diversao.component.ts
+++ src/app/diversao/diversao.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Oferta } from '../shared/ofertas.model';
+import { OfertasServices } from '../ofertas.service';
 
 @Component({
   selector: 'app-diversao',
   templateUrl: './diversao.component.html',
-  styleUrls: ['./diversao.component.css']
+  styleUrls: ['./diversao.component.css'],
+  providers: [ OfertasServices ]
 })
 export class DiversaoComponent implements OnInit {
 
-  constructor() { }
+  public ofertas : Oferta[];
+
+  constructor(private ofertasServices: OfertasServices) { }
 
   ngOnInit() {
+    this.ofertasServices.getOfertasPoCategoria('diversao').then((ofertas: Oferta[]) => this.ofertas = ofertas);
   }
 
 }
