6ac61a5d67581111dfe8e4eba7a551d56667e270
Felipe Marchant <felipemarchant@outlook.com>
2018-07-14 21:13:16 -0300

    Add Export Variable

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -1,6 +1,7 @@
 import { Oferta } from './shared/ofertas.model';
 import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
+import { URL_API } from './app.api';
 import 'rxjs';
 
 @Injectable()
@@ -9,18 +10,18 @@ export class OfertasServices {
     constructor (private http: Http){}
 
     public async getOfertas(){
-        return this.http.get('http://localhost:3000/ofertas?destaque=true')
+        return this.http.get(`${URL_API}?destaque=true`)
             .toPromise()
             .then(resp =>  resp.json());
     }
 
     public getOfertasPoCategoria(categoria: string): Promise<Oferta[]> {
-        return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+        return this.http.get(`${URL_API}?categoria=${categoria}`)
                  .toPromise()
                  .then(resp => resp.json());
     }
 
     public getOfertaPorId(id: number): Promise<Oferta>{
-        return this.http.get(`http://localhost:3000/ofertas?id=${id}`).toPromise().then((resp) => resp.json().shift()); 
+        return this.http.get(`${URL_API}?id=${id}`).toPromise().then((resp) => resp.json().shift()); 
     }
 }
