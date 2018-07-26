34129ceda368e20e5df16237bea31ae89808293a
Felipe Marchant <felipemarchant@outlook.com>
2018-07-25 23:50:27 -0300

    Listar em um list group

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { OfertasServices } from '../ofertas.service';
 import { Oferta }  from '../shared/ofertas.model';
-import { Observable } from 'rxjs';
+import { Observable, Subject, of } from 'rxjs';
+import { switchMap, debounceTime, distinctUntilChanged, catchError } from '../../../node_modules/rxjs/operators';
 
 @Component({
   selector: 'app-topo',
@@ -12,14 +13,40 @@ import { Observable } from 'rxjs';
 export class TopoComponent implements OnInit {
 
   public ofertas: Observable<Oferta[]>;
+  public list_ofertas: Oferta[];
+  private subjectPesquisa: Subject<string> = new Subject<string>();
 
   constructor(private ofertasService : OfertasServices) { }
 
   ngOnInit() {
+    this.ofertas = this.subjectPesquisa.pipe(
+      debounceTime(1000),
+      distinctUntilChanged(),
+      switchMap((termo: string) => {
+        if(termo.trim() === ''){
+          return of<Oferta[]>([]);
+        }
+
+        return this.ofertasService.pesquisaOfertas(termo);
+      })
+    );
+
+    this.ofertas.subscribe( (ofertas: Oferta[]) => {
+      console.log(ofertas);
+      this.list_ofertas = ofertas;
+    });
   }
 
-  pesquisa (termoDaBusca: string): void{
-    this.ofertasService.pesquisaOfertas(termoDaBusca)
+  public pesquisa (termoDaBusca: string): void{
+        
+    /*this.ofertas = this.ofertasService.pesquisaOfertas(termoDaBusca);
+    this.ofertas.subscribe(
+      (oferta: Oferta[]) => console.log(oferta),
+      () => console.log('Fluxo de Stream OK!')
+    );*/
+
+    this.subjectPesquisa.next(termoDaBusca);
+
   }
 
 }
