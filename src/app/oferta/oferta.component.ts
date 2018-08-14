18f93f7817e2dc028f6de23c2d88eb60938e722f
Felipe Marchant <felipemarchant@outlook.com>
2018-08-14 00:15:23 -0300

    Adaptando aplicação

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -2,26 +2,33 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { OfertasServices } from '../ofertas.service';
 import { Oferta } from '../shared/ofertas.model';
+import CarrinhoService from '../carrinho.service';
 
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
   styleUrls: ['./oferta.component.css'],
-  providers: [ OfertasServices ]
+  providers: [ OfertasServices, CarrinhoService ]
 })
 export class OfertaComponent implements OnInit {
 
   public oferta: Oferta;
 
   constructor(
-    private route: ActivatedRoute, private ofertasServices: OfertasServices
+    private route: ActivatedRoute, private ofertasServices: OfertasServices,
+    private carrinhoService: CarrinhoService
   ) {}
 
   ngOnInit() {
-
+    console.log(this.carrinhoService.exibirItens());
     this.route.params.subscribe((parametros:Params) => {
       this.ofertasServices.getOfertaPorId(parametros.id).then((oferta:Oferta) => this.oferta = oferta);
     });
+  }
+
+  public adicionarItemCarrinho():void 
+  {
+    this.carrinhoService.incluirItem(this.oferta);
   }
 
 }
