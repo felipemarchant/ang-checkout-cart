985517db06b76522693e9ef2f3c8dfebd16b3a3b
Felipe Marchant <felipemarchant@outlook.com>
2018-08-15 22:12:13 -0300

    Adicionando operações do carrinho

--- src/app/carrinho.service.ts
+++ src/app/carrinho.service.ts
@@ -18,7 +18,57 @@ class CarrinhoService
             oferta.valor,
             1
         );
-        this.itens.push(itemCarrinho);
+
+        let itemCarrinhoExiste = this.itens.find((item: ItemCarrinho) => item.id === itemCarrinho.id);
+
+        if (itemCarrinhoExiste)
+        {
+            itemCarrinhoExiste.quantidade += 1;
+        }
+        else
+        {
+            this.itens.push(itemCarrinho);
+        }
+
+    }
+    public totalCarrinhoCompras():number
+    {
+        let total:number = 0;
+        this.itens.map((item: ItemCarrinho) =>{
+            total += total + (item.valor * item.quantidade);
+        });
+        return total;
+
+    }
+
+    public adicionarQuantidade(item: ItemCarrinho):void
+    {
+        let itemCarrinho = this.itens.find((itemC :ItemCarrinho) => item.id === itemC.id );
+
+        if (itemCarrinho)
+        {
+            itemCarrinho.quantidade += 1;
+        }
+
+    }
+
+    public removerQuantidade(item: ItemCarrinho):void
+    {
+        let itemCarrinho = this.itens.find((itemC :ItemCarrinho) => item.id === itemC.id );
+
+        if (itemCarrinho)
+        {
+            if (itemCarrinho.quantidade < 2)
+            {
+                var index = this.itens.indexOf(itemCarrinho);
+                if (index !== -1) this.itens.splice(index, 1);
+            }
+            else
+            {
+                itemCarrinho.quantidade -= 1;
+            }
+        }
+
     }
 }
 
